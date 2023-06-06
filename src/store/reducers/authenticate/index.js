b4eb43d112a19000c746eb7904c872808cbc41c6
nagendra <nagendra.chauhan2001@gmail.com>
2023-06-06 09:06:34 +0530

    portfolio guest details added

--- src/store/reducers/authenticate/index.js
+++ src/store/reducers/authenticate/index.js
@@ -1,6 +1,6 @@
 import { authenticateConstants } from './actions';
 
-export default function authenticate(state = { userData: [{name: 'admin', email: 'admin@g.com', password: '12345678', confirmPassword: '12345678', id: 1}], authenticateUser: null }, action) {
+export default function authenticate(state = { userData: [{name: 'admin', email: 'admin@g.com', password: '12345678', confirmPassword: '12345678', id: 1},{name: 'guest', email: 'guest@g.com', password: '12345678', confirmPassword: '12345678', id: 2}], authenticateUser: null }, action) {
     switch (action.type) {
         case authenticateConstants.UPDATE_USER_LIST:
             return { ...state, userData: action.payload };
