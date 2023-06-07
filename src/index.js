677fce4d24bdcbcbbdbb0a908c473278dc70346c
nagendra <nagendra.chauhan2001@gmail.com>
2023-06-07 07:42:13 +0530

    portfolio basename test

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/reactPractice'>
+    <BrowserRouter  basename='/reactPractice'>
       <Provider store={store}>
         <App />
       </Provider>
